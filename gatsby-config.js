24aa1b3fc04c3e05af205387effeda47e6c53030
YushiOMOTE <yushiomote@gmail.com>
2020-10-11 16:46:44 +0900

    Remove unused gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,5 @@ require("dotenv").config({
 
 module.exports = {
     pathPrefix: process.env.THUMBNAIL_PATH_PREFIX || "/",
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: process.env.THUMBNAIL_IMAGE_PATH,
-            },
-        },
-    ],
+    plugins: [],
 }
