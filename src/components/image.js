7e119c1e1c81948a7464a1b9f465d74b931d98df
YushiOMOTE <yushiomote@gmail.com>
2020-12-25 09:35:37 +0900

    Add sorting feature

--- src/components/image.js
+++ src/components/image.js
@@ -14,6 +14,7 @@ import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import SearchIcon from '@material-ui/icons/Search';
 
 import MenuItem from '@material-ui/core/MenuItem';
@@ -48,24 +49,46 @@ query {
   }
 }`;
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+const SortMode = Object.freeze({ name: '1', lastused: '2', random: '3' });
+
 export default class Thambnail extends React.Component {
     constructor() {
         super();
 
         this.defaultPageSize = 200;
-        this.state = { large: true, keyword: "", page: 1, pageSize: this.defaultPageSize };
+        this.defaultSortMode = SortMode.name;
+        this.state = { large: true, keyword: "", page: 1, pageSize: this.defaultPageSize, sortMode: this.defaultSortMode, shuffled: [] };
         this.onSwitch = this.onSwitch.bind(this);
         this.onSearch = this.onSearch.bind(this);
         this.onClickSearch = this.onClickSearch.bind(this);
         this.onPageChange = this.onPageChange.bind(this);
         this.onChangePageSize = this.onChangePageSize.bind(this);
+        this.onChangeSortMode = this.onChangeSortMode.bind(this);
+        this.onClickShuffle = this.onClickShuffle.bind(this);
     }
 
     componentDidMount() {
         this.defaultPageSize = 200;
+        this.defaultSortMode = SortMode.name;
         var pageSize = localStorage.getItem('pageSize') || this.defaultPageSize;
         var large = (localStorage.getItem('large') || "true") === "true";
-        this.setState({ pageSize, large })
+        var sortMode = localStorage.getItem('sortMode') || this.defaultSortMode;
+        this.setState({ pageSize, large, sortMode })
     }
 
     onSwitch(event) {
@@ -83,7 +106,6 @@ export default class Thambnail extends React.Component {
     }
 
     onPageChange(event, page) {
-        console.log(page);
         this.setState({ page })
     }
 
@@ -92,13 +114,55 @@ export default class Thambnail extends React.Component {
         this.setState({ pageSize: event.target.value, page: 1 })
     }
 
+    onChangeSortMode(event) {
+        localStorage.setItem('sortMode', event.target.value);
+        this.setState({ sortMode: event.target.value, page: 1 })
+    }
+
+    onClickShuffle(event) {
+        var shuffled = shuffle(this.state.shuffled);
+        this.setState({ shuffled });
+    }
+
     render() {
         return (
             <div style={{width: "95%", margin: "auto"}}>
                 <StaticQuery
                     query={query}
                     render={data => {
-                        var items = data.allStamp.edges.filter(({node}, index) => {
+                        var edges = [...data.allStamp.edges];
+                        var with_shuffle = (<div/>);
+                        switch (this.state.sortMode) {
+                        case SortMode.name:
+                            edges.sort(function(a, b) {
+                                return a.node.filename - b.node.filename;
+                            });
+                            break;
+                        case SortMode.lastused:
+                            var time = JSON.parse(localStorage.getItem('time')) || {}
+                            edges.sort(function(a, b) {
+                                var d = (time[b.node.filename] || "0") - (time[a.node.filename] || "0")
+                                return d
+                            });
+                            break;
+                        case SortMode.random:
+                            if (this.state.shuffled.length == 0) {
+                                var shuffled = shuffle(edges);
+                                edges = shuffled;
+                                this.setState({ shuffled });
+                            } else {
+                                edges = this.state.shuffled;
+                            }
+                            with_shuffle = (
+                                <Grid item>
+                                  <Button variant="contained" color="secondary" onClick={this.onClickShuffle}>
+                                    Shuffle
+                                  </Button>
+                                </Grid>);
+                            break;
+                        }
+
+                        var items = edges.filter(({node}, index) => {
                             if (this.state.keyword === "") {
                                 return true
                             } else {
@@ -155,15 +219,32 @@ export default class Thambnail extends React.Component {
                                     </IconButton>
                                 </Paper>
 
-                                <Select
-                                    defaultValue={this.defaultPageSize}
-                                    value={this.state.pageSize}
-                                    onChange={this.onChangePageSize}>
-                                    <MenuItem value={50}>50</MenuItem>
-                                    <MenuItem value={100}>100</MenuItem>
-                                    <MenuItem value={200}>200</MenuItem>
-                                    <MenuItem value={400}>400</MenuItem>
-                                </Select>
+                                <Grid container component="label" spacing={1} alignItems="center">
+                                  <Grid item>PageSize</Grid>
+                                  <Grid item>
+                                    <Select
+                                      defaultValue={this.defaultPageSize}
+                                      value={this.state.pageSize}
+                                      onChange={this.onChangePageSize}>
+                                      <MenuItem value={50}>50</MenuItem>
+                                      <MenuItem value={100}>100</MenuItem>
+                                      <MenuItem value={200}>200</MenuItem>
+                                      <MenuItem value={400}>400</MenuItem>
+                                    </Select>
+                                  </Grid>
+                                  <Grid item>Sort</Grid>
+                                  <Grid item>
+                                    <Select
+                                      defaultValue={this.defaultSortMode}
+                                      value={this.state.sortMode}
+                                      onChange={this.onChangeSortMode}>
+                                      <MenuItem value={SortMode.name}>Name</MenuItem>
+                                      <MenuItem value={SortMode.lastused}>LastUsed</MenuItem>
+                                      <MenuItem value={SortMode.random}>Random</MenuItem>
+                                    </Select>
+                                  </Grid>
+                                  {with_shuffle}
+                                </Grid>
 
                                 <Pagination style={{display: "flex", justifyContent: "center", margin: "20px"}} count={pages} page={page} variant="outlined" color="primary" onChange={this.onPageChange} />
 
@@ -222,6 +303,9 @@ class Image extends React.Component {
 
         function onCopy(name) {
             return () => {
+                var time = JSON.parse(localStorage.getItem('time')) || {};
+                time[name] = Date.now().toString();
+                localStorage.setItem('time', JSON.stringify(time))
                 toast.info("Copied " + name, {autoClose: 2000});
             };
         }
