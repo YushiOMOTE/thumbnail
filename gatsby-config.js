602d7c6156cddccfeee41d5d6e0a7f37561185a1
YushiOMOTE <yushiomote@gmail.com>
2021-01-26 18:18:01 +0900

    Add electron

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,24 @@ require("dotenv").config({
 })
 
 module.exports = {
+    siteMetadata: {
+        title: process.env.THUMBNAIL_TITLE || `Thumbnail`,
+        description: `GIF image thumbnail where you can copy the image URL to clipboard by just clicking images.`,
+        author: `@yushiomote`,
+    },
     pathPrefix: process.env.THUMBNAIL_PATH_PREFIX || "/",
-    plugins: [],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`,
+            },
+        },
+    ],
 }
