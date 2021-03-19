6d366a15ca8f1a28b5a9baf350051e1ce8669831
YushiOMOTE <yushiomote@gmail.com>
2021-03-20 06:48:13 +0900

    Change fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import Thumbnail from "../components/thumbnail"
 import { Helmet } from "react-helmet"
+import "./index.css"
 
 export default function Home() {
   const [base, setBase] = useState(process.env.THUMBNAIL_IMAGE_BASE_URL)
