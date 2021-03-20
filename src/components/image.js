87714438f936d2fa63dada0eb84a293396723eaa
YushiOMOTE <yushiomote@gmail.com>
2021-03-20 19:07:16 +0900

    Add dark mode

--- src/components/image.js
+++ src/components/image.js
@@ -1,31 +1,25 @@
 import React from "react"
 import CopyToClipboard from "react-copy-to-clipboard"
-import { makeStyles } from "@material-ui/core/styles"
-import { Box } from "@material-ui/core"
-import ReactToolTip from "react-tooltip"
+import Tooltip from "@material-ui/core/Tooltip"
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    width: "100px",
-    height: "100px",
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    backgroundColor: "transparent",
-    borderRadius: "10%",
-    border: "none",
-    outline: "none",
-  },
-}))
+const style = {
+  width: "100px",
+  height: "100px",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundColor: "transparent",
+  borderRadius: "10%",
+  border: "none",
+  outline: "none",
+}
 
 function removeExtraSlash(s) {
   return s.replace(/([^:]\/)\/+/g, "$1")
 }
 
 export default function Image(props) {
-  const classes = useStyles()
-
-  var url = removeExtraSlash(props.url)
+  const url = removeExtraSlash(props.url)
 
   const onMouseOver = e => {
     e.currentTarget.style.border = "2px solid #008cba"
@@ -35,17 +29,16 @@ export default function Image(props) {
     e.currentTarget.style.border = "none"
   }
 
-  var markdown = url.replace(props.regex, props.replace)
+  const markdown = url.replace(props.regex, props.replace)
 
   return (
-    <Box>
+    <Tooltip title={props.filename}>
       <CopyToClipboard
         text={markdown}
         onCopy={() => props.onCopy(props.filename)}
       >
         <button
-          className={classes.button}
-          style={{ backgroundImage: "url(" + props.url + ")" }}
+          style={{ ...style, backgroundImage: "url(" + props.url + ")" }}
           aria-label="copy"
           data-tip={props.filename}
           onMouseOver={onMouseOver}
@@ -54,7 +47,6 @@ export default function Image(props) {
           onBlur={onMouseOut}
         />
       </CopyToClipboard>
-      <ReactToolTip delayShow={500} />
-    </Box>
+    </Tooltip>
   )
 }
